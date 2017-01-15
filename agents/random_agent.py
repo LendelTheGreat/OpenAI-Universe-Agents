c6541c2e445eeb37fff18f798dc36c14c719ade3
Tomas <tomaslanger@screen6.io>
2017-01-15 15:25:35 +0100

    random agent bugfix 2

--- agents/random_agent.py
+++ agents/random_agent.py
@@ -4,6 +4,9 @@ import universe  # register the universe environments
 from universe import wrappers
 from random import randint
 
+def sample_action():
+    return actions[randint(0, len(actions)-1)]
+
 env = gym.make('internet.SlitherIO-v0')
 env.configure(remotes='vnc://localhost:5900+15900')
 env = wrappers.experimental.SafeActionSpace(env)
@@ -15,6 +18,3 @@ while True:
     action_n = [[sample_action()] for ob in observation_n]
     observation_n, reward_n, done_n, info = env.step(action_n)
     env.render()
-
-def sample_action():
-    return actions[randint(0, len(actions)-1)]
