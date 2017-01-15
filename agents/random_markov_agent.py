061c0ff9f99ff39a3de816ba32ee87b35ae56cc1
Tomas <tomaslanger@screen6.io>
2017-01-15 15:52:44 +0100

    random markov agent

--- agents/random_markov_agent.py
+++ agents/random_markov_agent.py
@@ -2,13 +2,23 @@
 import gym
 import universe  # register the universe environments
 from universe import wrappers
+from random import randint
 
 env = gym.make('internet.SlitherIO-v0')
 env.configure(remotes='vnc://localhost:5900+15900')
 env = wrappers.experimental.SafeActionSpace(env)
 observation_n = env.reset()
+actions = [('KeyEvent', 'left', True), ('KeyEvent', 'right', True), ('KeyEvent', 'up', True)]
+
+action = 0
 
 while True:
-  action_n = [[('KeyEvent', 'ArrowUp', True)] for ob in observation_n]
-  observation_n, reward_n, done_n, info = env.step(action_n)
-  env.render()
+    r = randint(0, 9)
+    if r = 8:
+        action = (action + 1) % 3
+    elif r = 9:
+        action = (action - 1) % 3
+
+    action_n = [[actions[action]] for ob in observation_n]
+    observation_n, reward_n, done_n, info = env.step(action_n)
+    env.render()
